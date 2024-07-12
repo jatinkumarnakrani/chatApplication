7d9345e5bbbbf15a24f46b7830125085d60f2810
Jatinkumar Nakrani <nakranijatinkumar@gmail.com>
2024-07-12 17:12:51 +0200

    new Chat web application create.

---
+++ src/main/java/com/chat/app/chatapplication/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.chat.app.chatapplication.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/chat/app/chatapplication/config/WebSocketEventListener.java
@@ -0,0 +1,30 @@
+package com.chat.app.chatapplication.config;
+
+import com.chat.app.chatapplication.chat.ChatMessage;
+import com.chat.app.chatapplication.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User {} disconnected", username);
+            var chatMessage = ChatMessage.builder().type(MessageType.LEAVE).sender(username).build();
+            messagingTemplate.convertAndSend("/topic/public/", chatMessage);
+        }
+    }
+}
